5d628e65aa3f0c5cca1d51e5eb86614db29e6dd0
SevaBelyaev <seva.belyaev04@gmail.com>
2026-04-20 21:11:34 +0300

    test 3

--- firstlab/src/App.tsx
+++ firstlab/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>CI/CD Test 2</h1>
+          <h1>CI/CD Test 3</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
